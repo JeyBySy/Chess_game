9b2f18c634f80b9aaf151b82879ab9e5674823e8
JeyBySy <njcocosa@gmail.com>
2022-04-27 20:29:08 +0800

    Commit

--- public/js/Pieces.js
+++ public/js/Pieces.js
@@ -32,58 +32,65 @@ function buildPiece(id, x, y) {
     //========================= BLACK
     // PAWN
     if (id <= 16 && id >= 9) {
-        return document.getElementById(id).innerHTML += `<div onmousedown="drag(event)" class="p_black pieces" data-pos="${x + y}" data-color="black" data-piece="pawn">${PAWN};</div>`
+        return document.getElementById(id).innerHTML += `<div onclick="drag(event)" class="p_black pieces" data-pos="${x + y}" data-color="black" data-piece="pawn">${PAWN};</div>`
     }
     // ROOK
     if (id == 1 || id == 8) {
-        return document.getElementById(id).innerHTML += `<div onmousedown="drag(event)" class="p_black pieces" data-pos="${x + y}" data-color="black" data-piece="rook">${ROOK};</div>`
+        return document.getElementById(id).innerHTML += `<div onclick="drag(event)" class="p_black pieces" data-pos="${x + y}" data-color="black" data-piece="rook">${ROOK};</div>`
     }
     // KNGIHT
     if (id == 2 || id == 7) {
-        return document.getElementById(id).innerHTML += `<div onmousedown="drag(event)" class="p_black pieces" data-pos="${x + y}" data-color="black" data-piece="knight">${KNIGHT};</div>`
+        return document.getElementById(id).innerHTML += `<div onclick="drag(event)" class="p_black pieces" data-pos="${x + y}" data-color="black" data-piece="knight">${KNIGHT};</div>`
     }
     // BISHOP
     if (id == 3 || id == 6) {
-        return document.getElementById(id).innerHTML += `<div onmousedown="drag(event)" class="p_black pieces" data-pos="${x + y}" data-color="black" data-piece="bishop">${BISHOP};</div>`
+        return document.getElementById(id).innerHTML += `<div onclick="drag(event)" class="p_black pieces" data-pos="${x + y}" data-color="black" data-piece="bishop">${BISHOP};</div>`
     }
     // QUEEN
     if (id == 4) {
-        return document.getElementById(id).innerHTML += `<div onmousedown="drag(event)" class="p_black pieces" data-pos="${x + y}" data-color="black" data-piece="queen">${QUEEN};</div>`
+        return document.getElementById(id).innerHTML += `<div onclick="drag(event)" class="p_black pieces" data-pos="${x + y}" data-color="black" data-piece="queen">${QUEEN};</div>`
     }
     // KING
     if (id == 5) {
-        return document.getElementById(id).innerHTML += `<div onmousedown="drag(event)" class="p_black pieces" data-pos="${x + y}" data-color="black" data-piece="king">${KING};</div>`
+        return document.getElementById(id).innerHTML += `<div onclick="drag(event)" class="p_black pieces" data-pos="${x + y}" data-color="black" data-piece="king">${KING};</div>`
     }
 
     //========================= WHITE
     // PAWN
     if (id <= 56 && id >= 49) {
-        return document.getElementById(id).innerHTML += `<div onmousedown="drag(event)" class="p_white pieces"  data-pos="${x + y}" data-color="white" data-piece="pawn"">${PAWN};</div>`
+        return document.getElementById(id).innerHTML += `<div onclick="drag(event)" class="p_white pieces"  data-pos="${x + y}" data-color="white" data-piece="pawn"">${PAWN};</div>`
     }
     // ROOK
     if (id == 57 || id == 64) {
-        return document.getElementById(id).innerHTML += `<div onmousedown="drag(event)" class="p_white pieces" data-pos="${x + y}" data-color="white" data-piece="rook">${ROOK};</div>`
+        return document.getElementById(id).innerHTML += `<div onclick="drag(event)" class="p_white pieces" data-pos="${x + y}" data-color="white" data-piece="rook">${ROOK};</div>`
     }
     // KNIGHT
     if (id == 58 || id == 63) {
-        return document.getElementById(id).innerHTML += `<div onmousedown="drag(event)" class="p_white pieces" data-pos="${x + y}" data-color="white" data-piece="knight">${KNIGHT};</div>`
+        return document.getElementById(id).innerHTML += `<div onclick="drag(event)" class="p_white pieces" data-pos="${x + y}" data-color="white" data-piece="knight">${KNIGHT};</div>`
     }
     // BISHOP
     if (id == 59 || id == 62) {
-        return document.getElementById(id).innerHTML += `<div onmousedown="drag(event)" class="p_white pieces" data-pos="${x + y}" data-color="white" data-piece="bishop">${BISHOP};</div>`
+        return document.getElementById(id).innerHTML += `<div onclick="drag(event)" class="p_white pieces" data-pos="${x + y}" data-color="white" data-piece="bishop">${BISHOP};</div>`
     }
     // QUEEN
     if (id == 60) {
-        return document.getElementById(id).innerHTML += `<div onmousedown="drag(event)" class="p_white pieces" data-pos="${x + y}" data-color="white" data-piece="queen">${QUEEN};</div>`
+        return document.getElementById(id).innerHTML += `<div onclick="drag(event)" class="p_white pieces" data-pos="${x + y}" data-color="white" data-piece="queen">${QUEEN};</div>`
     }
     // KING
     if (id == 61) {
-        return document.getElementById(id).innerHTML += `<div onmousedown="drag(event)" class="p_white pieces" data-pos="${x + y}" data-color="white" data-piece="king">${KING};</div>`
+        return document.getElementById(id).innerHTML += `<div onclick="drag(event)" class="p_white pieces" data-pos="${x + y}" data-color="white" data-piece="king">${KING};</div>`
     }
 }
 
 function drag(event) {
-    console.log(event)
+    const allPiece = document.getElementsByClassName('pieces')
+    for (let i = 0; i < allPiece.length; i++) {
+        if (allPiece[i].classList.contains('active')) {
+            allPiece[i].classList.remove('active')
+        } else {
+            event.target.classList.add('active')
+        }
+    }
 }
 
 // Initialized
